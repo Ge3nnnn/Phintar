241d36d0cf84876814c4429288252bcc2eeb1659
Ge3nnnn <fasle.kresna12@gmail.com>
2026-08-05 13:52:40 +0700

    Based_Phintar

---
+++ lib/loginpage/login.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class LoginPagePhintar extends StatelessWidget {
+  const LoginPagePhintar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold( backgroundColor: Color(0xFF0F172A),);
+  }
+}
