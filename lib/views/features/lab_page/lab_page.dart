b7007b746ddc775aa1947c52d78e99e81e85c54e
Ge3nnnn <fasle.kresna12@gmail.com>
2026-08-23 19:49:30 +0700

    widgets update

--- lib/views/features/lab_page/lab_page.dart
+++ lib/views/features/lab_page/lab_page.dart
@@ -4,7 +4,7 @@ import 'package:blabla/views/features/lab_page/labo/labo_bandul_matematis.dart';
 import 'package:blabla/views/features/lab_page/labo/labo_konstanta_pegas.dart';
 import 'package:blabla/widgets/app_banner.dart';
 import 'package:blabla/widgets/app_bar.dart';
-import 'package:blabla/widgets/app_textfield.dart';
+import 'package:blabla/widgets/app_search_bar.dart';
 import 'package:flutter/material.dart';
 
 // Model sederhana untuk data laboratorium
